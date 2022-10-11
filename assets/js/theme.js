08bef51e5b98058455e5f2caead676f7b5ecfcd7
kavindu-sayuranga <ksayuranga00@gmai.com>
2022-10-11 13:09:20 +0530

    project add

---
+++ assets/js/theme.js
@@ -0,0 +1,11 @@
+$(document).mousemove(function(event) {
+    // currentMousePos.x = event.pageX;
+    // currentMousePos.y = event.pageY;
+    console.log(event.pageY+"----"+event.pageX);
+
+    $('#mainCircle').css({
+        'top':event.pageY-20,
+        'left':event.pageX-20,
+    });
+
+});
