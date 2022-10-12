44fc216681f34a3455c6d76fffff799c1fde87bd
kavindu-sayuranga <ksayuranga00@gmai.com>
2022-10-12 20:34:23 +0530

    dashboard button click function pos

--- assignments/js/SPA/controller/indexController.js
+++ assignments/js/SPA/controller/indexController.js
@@ -3,3 +3,16 @@ $("#item").css("display", "none");
 $("#order").css("display", "none");
 
 $("#dashboardBtn").css("font-weight", "bold");
+
+$("#dashboardBtn").click(function () {
+    $("#customer").css("display", "none");
+    $("#item").css("display", "none");
+    $("#order").css("display", "none");
+    $("#home").css("display", "block");
+
+    $("#dashboardBtn").css("font-weight", "bold");
+    $("#orderBtn").css("font-weight", "normal");
+    $("#itemBtn").css("font-weight", "normal");
+    $("#customerBtn").css("font-weight", "normal");
+
+});
