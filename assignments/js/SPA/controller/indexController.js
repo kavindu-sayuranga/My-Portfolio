d398434c8ed9f0008eb7ee6f8d31fc77ba430928
kavindu-sayuranga <ksayuranga00@gmai.com>
2022-10-13 15:31:51 +0530

    cursor animation

--- assignments/js/SPA/controller/indexController.js
+++ assignments/js/SPA/controller/indexController.js
@@ -16,3 +16,42 @@ $("#dashboardBtn").click(function () {
     $("#customerBtn").css("font-weight", "normal");
 
 });
+
+$("#orderBtn").click(function () {
+    $("#customer").css("display", "none");
+    $("#item").css("display", "none");
+    $("#order").css("display", "block");
+    $("#home").css("display", "none");
+
+    $("#orderBtn").css("font-weight", "bold");
+    $("#dashboardBtn").css("font-weight", "normal");
+    $("#itemBtn").css("font-weight", "normal");
+    $("#customerBtn").css("font-weight", "normal");
+
+
+    generateOrderId();
+});
+
+$("#customerBtn").click(function () {
+    $("#customer").css("display", "block");
+    $("#item").css("display", "none");
+    $("#order").css("display", "none");
+    $("#home").css("display", "none");
+
+    $("#customerBtn").css("font-weight", "bold");
+    $("#orderBtn").css("font-weight", "normal");
+    $("#dashboardBtn").css("font-weight", "normal");
+    $("#itemBtn").css("font-weight", "normal");
+});
+
+$("#itemBtn").click(function () {
+    $("#customer").css("display", "none");
+    $("#item").css("display", "block");
+    $("#order").css("display", "none");
+    $("#home").css("display", "none");
+
+    $("#customerBtn").css("font-weight", "normal");
+    $("#orderBtn").css("font-weight", "normal");
+    $("#dashboardBtn").css("font-weight", "normal");
+    $("#itemBtn").css("font-weight", "bold");
+});
